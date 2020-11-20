a9205c4e35b5fcb8316d79fb797dc71e4d2a31e2
ch <mkmsacc@outlook.com>
2020-11-20 23:41:45 +0700

    Create mockito test

---
+++ src/main/java/AccountManager.java
@@ -0,0 +1,8 @@
+
+
+public interface AccountManager {
+	Account findAccountForUser(String userId);
+
+	void updateAccount(Account account);
+
+}
---
+++ src/test/java/AccountServiceTestMockito.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTestMockito {
+
+	@Mock
+	private AccountManager mockAccountManager;
+
+	@Test
+	public void testTransferOk() {
+		Account senderAccount = new Account("1", 200);
+		Account beneficiaryAccount = new Account("2", 100);
+
+		Mockito.lenient().when(mockAccountManager.findAccountForUser("1"))
+				.thenReturn(senderAccount);
+		Mockito.lenient().when(mockAccountManager.findAccountForUser("2"))
+				.thenReturn(beneficiaryAccount);
+		
+		AccountService accountService = new AccountService();
+		accountService.setAccountManager(mockAccountManager);
+		accountService.transfer("1", "2", 50);
+		
+		assertEquals(150, senderAccount.getBalance());
+		assertEquals(150, beneficiaryAccount.getBalance());
+	}
+
+}
